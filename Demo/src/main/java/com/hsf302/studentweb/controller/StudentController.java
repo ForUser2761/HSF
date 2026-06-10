6867ac656dd0b4b090b9390e023754692180b32f
foruser <vinhpham2761.gmail.com>
2026-06-10 09:03:29 +0700

    Form create

--- Demo/src/main/java/com/hsf302/studentweb/controller/StudentController.java
+++ Demo/src/main/java/com/hsf302/studentweb/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.hsf302.studentweb.controller;
 
+import com.hsf302.studentweb.entity.Student;
 import com.hsf302.studentweb.repository.StudentRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -20,6 +23,18 @@ public class StudentController {
     public String listStudent(Model model) {
         model.addAttribute("students", studentRepository.findAll());
         return "student-list";
+    }
+
+    @GetMapping("/students/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("student", new Student());
+        return "student-form";
+    }
+
+    @PostMapping("/students/add")
+    public String addStudent(@ModelAttribute Student student) {
+        studentRepository.save(student);
+        return "redirect:/students";
     }
 
 }
